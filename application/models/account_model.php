767e8b0e439852a6c51fa22f6c6b3ebeb9ea6e50
Jirayu Kanda <i.object@me.com>
2014-01-26 22:08:10 +0700

    edit bug send mail

--- application/models/account_model.php
+++ application/models/account_model.php
@@ -1670,11 +1670,11 @@ class account_model extends CI_Model
 		$data['account_confirm_code'] = random_string('alnum', '6');
 		
 		// send register email
-		$send_result = $this->sendRegisterEmail($data);
-		if ($send_result !== true) {
-			return $send_result;
-		}
-		unset($send_result);
+		// $send_result = $this->sendRegisterEmail($data);
+		// if ($send_result !== true) {
+		// 	return $send_result;
+		// }
+		// unset($send_result);
 		
 		// load date helper for gmt
 		$this->load->helper('date');
